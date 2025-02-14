938390fe1d673d6c941e1229413226054eb3d758
Parasonic <kiriattila11@gmail.com>
2025-02-14 16:28:54 +0100

    The base template for our project but needs to be edited

---
+++ src/noa/train.py
@@ -0,0 +1,10 @@
+from noa.model import Model
+from noa.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
